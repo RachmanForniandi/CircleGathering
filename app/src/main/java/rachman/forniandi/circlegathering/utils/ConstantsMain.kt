b55575d27aa0b7952cd79ed5d2848ed35f1fd15a
RachmanForniandi <rachmanforniandi@gmail.com>
2023-09-07 12:04:20 +0700

    added listener glide.

--- app/src/main/java/rachman/forniandi/circlegathering/utils/ConstantsMain.kt
+++ app/src/main/java/rachman/forniandi/circlegathering/utils/ConstantsMain.kt
@@ -2,10 +2,14 @@ package rachman.forniandi.circlegathering.utils
 
 import android.annotation.SuppressLint
 import android.graphics.drawable.Drawable
+import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
 import kotlinx.coroutines.flow.Flow
@@ -48,12 +52,33 @@ class ConstantsMain {
                         .placeholder(R.drawable.place_holder)
                         .error(R.drawable.error_placeholder)
                         .into(imageView)*/
-                Glide.with(imageView)
+                Glide.with(imageView.context)
                     .load(imgUrl)
-                    .timeout(15000)
-                    .fitCenter()
-                    .placeholder(R.drawable.place_holder)
-                    .error(R.drawable.error_placeholder)
+                    .centerCrop()
+                    .apply(RequestOptions()
+                        .placeholder(R.drawable.place_holder)
+                        .error(R.drawable.error_placeholder))
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            Log.d("aa","===${e}")
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable,
+                            model: Any,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            return false
+                        }
+                    })
                     .into(imageView)
                 }
             /*Picasso
