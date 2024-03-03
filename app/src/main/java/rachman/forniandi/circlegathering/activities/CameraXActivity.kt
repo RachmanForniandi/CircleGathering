39c4d76a0dd9490e22d174176c8c2d9f935f0f9e
RachmanForniandi <rachmanforniandi@gmail.com>
2024-03-03 08:52:28 +0700

    get input image from CameraX feature successful. Issue on submit to server.

--- app/src/main/java/rachman/forniandi/circlegathering/activities/CameraXActivity.kt
+++ app/src/main/java/rachman/forniandi/circlegathering/activities/CameraXActivity.kt
@@ -1,20 +1,27 @@
 package rachman.forniandi.circlegathering.activities
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.OrientationEventListener
 import android.view.Surface
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.*
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCapture.FLASH_MODE_OFF
 import androidx.camera.core.ImageCapture.FLASH_MODE_ON
 import androidx.camera.core.ImageCapture.FlashMode
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import rachman.forniandi.circlegathering.R
 import rachman.forniandi.circlegathering.databinding.ActivityCameraXBinding
+import rachman.forniandi.circlegathering.utils.createCustomTempFileImg
 
 class CameraXActivity : AppCompatActivity() {
 
@@ -28,13 +35,102 @@ class CameraXActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraXBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setFlashModeListener()
 
         binding.imgSwitchPositionCamera.setOnClickListener {
             cameraSelector =
                 if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA
                 else CameraSelector.DEFAULT_BACK_CAMERA
+            initiateCamera()
+        }
+
+        binding.imgCaptureImage.setOnClickListener {
+            actionTakePhoto()
         }
     }
+
+    private fun actionTakePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        val photoFile = createCustomTempFileImg(application)
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val intent = Intent()
+                    intent.putExtra(EXTRA_CAMERAX_IMAGE, output.savedUri.toString())
+                    setResult(CAMERAX_RESULT, intent)
+                    finish()
+                }
+
+                override fun onError(exc: ImageCaptureException) {
+                    Toast.makeText(
+                        this@CameraXActivity,
+                        "Gagal mengambil gambar.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.e(TAG, "onError: ${exc.message}")
+                }
+            }
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        orientationEventListener.enable()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        orientationEventListener.disable()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+        initiateCamera()
+    }
+
+    private fun initiateCamera() {
+        val cameraProviderObject = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderObject.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderObject.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                val objCameraX=cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+                objCameraX.cameraInfo.hasFlashUnit()
+                objCameraX.cameraControl.enableTorch(true)
+
+            } catch (exc: Exception) {
+                Toast.makeText(
+                    this@CameraXActivity,
+                    "Gagal memunculkan kamera.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.e(TAG, "startCamera: ${exc.message}")
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+
+
 
     private fun hideSystemUI() {
         @Suppress("DEPRECATION")
@@ -59,9 +155,9 @@ class CameraXActivity : AppCompatActivity() {
                 binding.imgButtonFlash.setImageDrawable(getDrawable(R.drawable.ic_flash_off_black_48dp))
                 flashMode = FLASH_MODE_OFF
             }
-            //bindCamera()
         }
     }
+
 
     private val orientationEventListener by lazy {
         object : OrientationEventListener(this) {
