446d58a4ecdc16f8705b96c816ce42ad7a41f14f
RachmanForniandi <rachmanforniandi@gmail.com>
2025-06-14 21:14:04 +0700

    issue glitch list stories on main page Resolved.

--- app/src/main/java/rachman/forniandi/circlegathering/dBRoom/entities/StoriesEntity.kt
+++ app/src/main/java/rachman/forniandi/circlegathering/dBRoom/entities/StoriesEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "story_table")
-class StoriesEntity (
+data class StoriesEntity (
     @PrimaryKey
     val id: String,
     val name: String? = null,
